b777255297ddbc42b20d6cc7ca6825a6e9c566c7
Martin Michálek <michalek@shortcat.cz>
2016-03-29 22:09:04 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    grunticon: {
+     icons: {
+       files: [
+         {
+           expand: true,
+           cwd: 'src/img/svg',
+           src: ['*.svg'],
+           dest: 'dist/grunticon'
+        }   
+       ], 
+       options: {
+         enhanceSVG: true,
+         compressPNG: true,
+         datasvgcss: 'css/icons.css',
+         datapngcss: 'css/icons-png.css',
+         urlpngcss: 'css/icons-png-bg.css'
+       }       
+     }
+    }      
+  });
+
+  grunt.loadNpmTasks('grunt-grunticon');
+
+  grunt.registerTask('default', ['grunticon']);
+
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# Demo práce s Grunticon
+
+[Grunticon](http://www.grunticon.com/) je sada nástrojů pro maximalizaci zpětné kompatibility [SVG vektorových obrázků](svg.md).
+
+Viz:
+- [Gruntfile.js](blob/master/Gruntfile.js)
+- [index.html](blob/master/index.html)
+
+- [Článek na Vzhůru dolů](http://www.vzhurudolu.cz/prirucka/svg-grunticon)
+- [SVG školení](http://www.vzhurudolu.cz/kurzy/svg)
+
+
